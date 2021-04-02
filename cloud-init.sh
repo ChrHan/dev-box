21c9e554c1673daefb12f56329fdd7c1956f6947
Christoper Hans <christoper.hans@bukalapak.com>
2021-04-02 20:06:30 +0700

    add .vimrc

--- cloud-init.sh
+++ cloud-init.sh
@@ -3,10 +3,20 @@ set -euo pipefail
 
 USERNAME=chris # TODO: Customize the sudo non-root username here
 
+# Install packages
+apt-get install -y vim tmux zsh fonts-powerline
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
 # Create user and immediately expire password to force a change on login
 useradd --create-home --shell "/bin/bash" --groups sudo "${USERNAME}"
 passwd --delete "${USERNAME}"
 chage --lastday 0 "${USERNAME}"
+
+# Change shell to zsh
+usermod --shell $(which zsh) chris
+echo 'chris ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo
 
 # Create SSH directory for sudo user and move keys over
 home_directory="$(eval echo ~${USERNAME})"
@@ -16,6 +26,10 @@ chmod 0700 "${home_directory}/.ssh"
 chmod 0600 "${home_directory}/.ssh/authorized_keys"
 chown --recursive "${USERNAME}":"${USERNAME}" "${home_directory}/.ssh"
 
+# Copy .vimrc
+
+
 # Disable root SSH login with password
 sed --in-place 's/^PermitRootLogin.*/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
 if sshd -t -q; then systemctl restart sshd fi
+
